a501df69c30693b8a73423c7ecd91b0fb4ea1fa1
Cyrus Chan <cyruschan1212@gmail.com>
2022-05-03 21:59:55 +0800

    Deploy theme mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
+  if (!kIsWeb) {
+    throw UnsupportedError(
+        "This is a web application, and only run on browser.");
+  }
+
   await Hive.initFlutter();
   await Hive.openBox("setting");
 
@@ -15,18 +21,19 @@ class Rk0ccStatus extends StatelessWidget {
   Widget build(BuildContext context) => ValueListenableBuilder<Box>(
       valueListenable: Hive.box("setting").listenable(keys: ["theme_mode"]),
       builder: (context, box, child) => MaterialApp(
-            themeMode: box.get("dark_mode", defaultValue: false)
-                ? ThemeMode.dark
-                : ThemeMode.light,
-          ));
-}
-
-class Rk0ccStatusPage extends StatelessWidget {
-  const Rk0ccStatusPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => SafeArea(
-          child: Scaffold(
-        appBar: AppBar(title: Text("rk0cc.xyz API server status")),
-      ));
+          themeMode: box.get("dark_mode", defaultValue: false)
+              ? ThemeMode.dark
+              : ThemeMode.light,
+          home: SafeArea(
+              child: Scaffold(
+            appBar: AppBar(
+                title: Text("rk0cc.xyz API server status"),
+                actions: <IconButton>[
+                  IconButton(
+                      onPressed: () =>
+                          box.put("dark_mode", !box.get("dark_mode")),
+                      icon: Icon(Icons.dark_mode,
+                          semanticLabel: "Toggle dark mode"))
+                ]),
+          ))));
 }
